0dec362636e44c76c35ad0edab0ce08dee2ad333
25LP1749 25LP1749 <merkzadok@gmail.com>
2025-07-24 12:43:37 +0800

    a

--- src/app/(main)/_components/header/UserToolbar.tsx
+++ src/app/(main)/_components/header/UserToolbar.tsx
@@ -1,12 +1,20 @@
+import { toast } from "sonner";
 import { HeaderAddressSelectButton } from ".././header/HeaderAddressSelectButton";
 import { HeaderCartButton } from "..//header/HeaderCartButton";
 import { HeaderUserProfileIcon } from "..//header/HeaderUserProfileIcon";
+import { useUser } from "@/providers/UserProvider";
 
 type UserToolbarProps = {
   openSidebar: () => void;
 };
 
 export const UserToolbar = ({ openSidebar }: UserToolbarProps) => {
+  const { user } = useUser();
+
+  if (!user) {
+    console.log("Login хийнэ үү!");
+    toast.error(user);
+  }
   return (
     <>
       <HeaderAddressSelectButton />
